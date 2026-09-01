f18282c200561adef4851f12c7922665203fe7d5
Cristiano Souza <crstnsz@gmail.com>
2026-08-31 22:56:48 -0300

    corrindo para linux

--- .dotfiles/check_dll_corflags.zsh
+++ .dotfiles/check_dll_corflags.zsh
@@ -1,5 +1,5 @@
 function check-dll-core-flags() {
-    local cake_project="$HOME/.dotfiles/tasks/build.csproj"
+    local cake_project="$HOME/.dotfiles/tasks/Build.csproj"
     
     # Se o usuário não passou um diretório ($1), usa o diretório atual do terminal (.)
     local folder="${1:a}"
